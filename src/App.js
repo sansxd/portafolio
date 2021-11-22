d02e745a136224f3f016edf05486c5b9c0864850
sergio <sergioans95@hotmail.com>
2021-11-22 16:22:45 -0300

    no need

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import Header from "./components/Header";
 import Footer from "./components/Footer";
 import About from "./components/About";
 import Experience from "./components/Experience";
-// import Projects from "./components/Projects";
 import Skills from "./components/Skills";
 
 class App extends Component {
@@ -118,10 +117,6 @@ class App extends Component {
           resumeBasicInfo={this.state.resumeData.basic_info}
           sharedBasicInfo={this.state.sharedData.basic_info}
         />
-        {/* <Projects
-          resumeProjects={this.state.resumeData.projects}
-          resumeBasicInfo={this.state.resumeData.basic_info}
-        /> */}
         <Skills
           sharedSkills={this.state.sharedData.skills}
           resumeBasicInfo={this.state.resumeData.basic_info}
